468ff2b817fb9cf895d759234b2d9424e1064e5c
Jerome Quere <jquere@scaleway.com>
2020-08-24 12:30:01 +0200

    update comment

--- scaleway/version.go
+++ scaleway/version.go
@@ -1,4 +1,5 @@
 package scaleway
 
+// This variable is filled by the release script at build time
 // version not a const because we modify it in provider_test.go
-var version = "v1.16.0"
+var version = "v0.0.0"
